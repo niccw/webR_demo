cfc7e8d00066303519f7d67ac1e3e52912b0be68
Nicola Wong <w.wong@garvan.org.au>
2024-08-27 11:30:29 +1000

    Add httpuv example

--- shinyR/shinylive_demo.R
+++ shinyR/shinylive_demo.R
@@ -5,5 +5,9 @@ system.file("examples", "01_hello", package="shiny") |>
 # Export myapp to webR
 shinylive::export("myapp", "webR_site")
 
+# Export myapp to webR
+shinylive::export("app_uploader", "webR_site_uploader")
+
 # Serve from R
 #httpuv::runStaticServer("webR_site/")
+#httpuv::runStaticServer("webR_site_uploader/")
